0040ea1a603235220e729ad9276e6ae8a8f210a9
Shraman91 <shramandeshpande93@gmail.com>
2026-04-21 11:17:48 +0530

    final fix

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -27,9 +27,6 @@ const ProtectedRoute = ({ children }) => {
             />
             <path
               className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              style={{ color: '#ffffff' }}
             />
           </svg>
         </div>
@@ -59,7 +56,7 @@ const ProtectedRoute = ({ children }) => {
               className="opacity-75"
               fill="currentColor"
               d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              style={{ color: '#ffffff' }}
+              style={{ color: '#fa0b0b' }}
             />
           </svg>
         </div>
